ba2c5e386fd58a083f9e09d06428a3276086d837
otiagoluz <tiagoluzgpr@gmail.com>
2020-02-25 23:56:16 -0500

    first commit

---
+++ src/pages/sobre.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from '../components/Header'
+
+export default () => (
+  <div style={{ color: 'teal' }}>
+    <Header headerText='About Hatsby' />
+    <Header headerText='Such wow. Very React.'/>
+  </div>
+)
